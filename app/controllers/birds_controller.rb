ffc9caa4e79101a718363cb6d2f36428eee46164
Nana <nanaolomaiyani@gmail.com>
2022-11-24 13:44:08 +0300

    assignment done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,4 @@
 class BirdsController < ApplicationController
-
   # GET /birds
   def index
     birds = Bird.all
@@ -9,6 +8,7 @@ class BirdsController < ApplicationController
   # POST /birds
   def create
     bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -20,6 +20,10 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+
+  def bird_params
+    params.permit(:name, :species)
   end
+end
 
 end
